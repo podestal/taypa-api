8c3947c8361fa5a33613d4cf4f5bca1098d2bcd2
podestal <luisrodriguezpodesta@gmail.com>
2025-10-21 08:57:42 -0400

    core user

--- app/taypa/settings.py
+++ app/taypa/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'core',
+    'store'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
