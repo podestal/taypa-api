fbe3904d4dc7478397aeb919dbe8f9aa375ff105
podestal <luisrodriguezpodesta@gmail.com>
2025-12-01 16:07:44 -0500

    update nginx confi for ws

--- app/taypa/asgi.py
+++ app/taypa/asgi.py
@@ -11,7 +11,6 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taypa.settings.dev')
