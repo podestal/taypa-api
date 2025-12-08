b9653f1ccf802da0e0f466b8c637f757bfb17287
podestal <luisrodriguezpodesta@gmail.com>
2025-12-08 07:40:00 -0500

    image added to dish model

--- app/store/models.py
+++ app/store/models.py
@@ -27,6 +27,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='dishes/', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
